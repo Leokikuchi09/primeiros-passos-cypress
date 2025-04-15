9f0551dbf48847b7faadce6c454298e47d022419
Leokikuchi09 <leonardo_kikuchi@hotmail.com>
2025-04-15 20:34:10 -0300

    add user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,32 +1,37 @@
+import userData from 'd:/Development/Leonardo/Aula/primeiros-passos-cypress/cypress/fixtures/userData.json'
+
 describe('Orange HRM Test', () => {
 
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: '.oxd-alert',
     passwordRequired:'.oxd-input-field-error-message'
   }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
   })
+
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admm')
-    cy.get(selectorsList.passwordField).type('Admin12')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
     
   })
-  it('Login without Password', () => {
+
+  it('Login Without Password', () => {
 cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-cy.get(selectorsList.usernameField).type('Admin')
+cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
 cy.get(selectorsList.loginButton).click()
 cy.get(selectorsList.passwordRequired)
 })
