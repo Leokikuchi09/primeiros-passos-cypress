d723cb7a62c5104e5bee4acf3e5eb331b2551e85
Leokikuchi09 <leonardo_kikuchi@hotmail.com>
2025-04-16 19:46:55 -0300

    add more selectorsList

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,67 @@
+import userData from 'd:/Development/Leonardo/Aula/primeiros-passos-cypress/cypress/fixtures/userData.json'
+
+describe('Orange HRM Test', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: '.oxd-alert',
+    passwordRequired:'.oxd-input-field-error-message',
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastName: "[name='lastName']",
+    employeeIdField: ":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    otherIdField: ":nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    driverLicenceField: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    licenceExpireField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+    nacionalityField: ":nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input",
+    maritalField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input",
+    dateBirth: ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+    closeButton: ".--close",
+    saveButton: ":nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button"
+
+
+
+
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('Leonardo')
+    cy.get(selectorsList.lastName).clear().type('Kikuchi')
+    cy.get(selectorsList.employeeIdField).clear().type('0024')
+    cy.get(selectorsList.otherIdField).clear().type('1314')
+    cy.get(selectorsList.driverLicenceField).clear().type('1011121314')
+    cy.get(selectorsList.licenceExpireField).clear().type('2025-07-09')
+    cy.get(selectorsList.closeButton).click()
+    cy.get(selectorsList.saveButton).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+  
+
+  })
+
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+    
+  })
+
+  it('Login Without Password', () => {
+cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+cy.get(selectorsList.loginButton).click()
+cy.get(selectorsList.passwordRequired)
+})
+
+})
