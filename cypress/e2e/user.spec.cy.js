2d5f01a7e5e9b067da5481a998233a4a63853259
Leokikuchi09 <leonardo_kikuchi@hotmail.com>
2025-04-24 18:50:01 -0300

    add gitignore and removing node_modules/package-lock

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,7 +5,10 @@ import MenuPage from '../fixtures/pages/menuPage'
 import MyInfoPage from '../fixtures/pages/myInfoPage'
 import LoginFail from '../fixtures/pages/loginFail'
 
+const Chance = require('chance')
 
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -18,9 +21,9 @@ describe('Orange HRM Test', () => {
   it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
-    DashboardPage.checkDashboardPage()
+    dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
-    myInfoPage.PersonalDetails('First Name', 'Last Name')
+    myInfoPage.PersonalDetails(chance.first(), chance.last())
     myInfoPage.EmployeeDetails('EmployId', 'OtherId', '10111213', '2025-10-07')
     myInfoPage.status()
     myInfoPage.saveForm()
